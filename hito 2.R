1af0276543e775ba1ce43556d22255c0a7ac8149
JunTierSS <135179973+JunTierSS@users.noreply.github.com>
2023-11-03 00:35:43 -0300

    Add files via upload

--- hito 2.R
+++ hito 2.R
@@ -13,10 +13,10 @@ Rendimiento2021Filtrado <- Rendimiento2021  %>% filter(COD_REG_RBD == '13') %>% 
 Rendimiento2022Filtrado <- Rendimiento2022  %>% filter(COD_REG_RBD == '13') %>% filter(NOM_COM_RBD == 'SANTIAGO') %>% 
   filter(COD_ENSE == '310')  %>% filter(COD_GRADO == '4') %>% filter(ASISTENCIA != '0')
 
-FRendimiento2019Filtrado <- Rendimiento2019Filtrado[c('AGNO','MRUN','RBD','NOM_RBD','NOM_COM_ALU','COD_GRADO','LET_CUR','COD_JOR','GEN_ALU','FEC_NAC_ALU','EDAD_ALU','PROM_GRAL','ASISTENCIA')]
-FRendimiento2020Filtrado <- Rendimiento2020Filtrado[c('AGNO','MRUN','RBD','NOM_RBD','NOM_COM_ALU','COD_GRADO','LET_CUR','COD_JOR','GEN_ALU','FEC_NAC_ALU','EDAD_ALU','PROM_GRAL','ASISTENCIA')]
-FRendimiento2021Filtrado <- Rendimiento2021Filtrado[c('AGNO','MRUN','RBD','NOM_RBD','NOM_COM_ALU','COD_GRADO','LET_CUR','COD_JOR','GEN_ALU','FEC_NAC_ALU','EDAD_ALU','PROM_GRAL','ASISTENCIA')]
-FRendimiento2022Filtrado <- Rendimiento2022Filtrado[c('AGNO','MRUN','RBD','NOM_RBD','NOM_COM_ALU','COD_GRADO','LET_CUR','COD_JOR','GEN_ALU','FEC_NAC_ALU','EDAD_ALU','PROM_GRAL','ASISTENCIA')]
+FRendimiento2019Filtrado <- Rendimiento2019Filtrado[c('AGNO','MRUN','RBD','NOM_RBD','NOM_COM_ALU','NOM_COM_RBD','COD_DEPE','COD_GRADO','RURAL_RBD','LET_CUR','COD_JOR','GEN_ALU','FEC_NAC_ALU','EDAD_ALU','PROM_GRAL','ASISTENCIA')]
+FRendimiento2020Filtrado <- Rendimiento2020Filtrado[c('AGNO','MRUN','RBD','NOM_RBD','NOM_COM_ALU','NOM_COM_RBD','COD_DEPE','COD_GRADO','RURAL_RBD','LET_CUR','COD_JOR','GEN_ALU','FEC_NAC_ALU','EDAD_ALU','PROM_GRAL','ASISTENCIA')]
+FRendimiento2021Filtrado <- Rendimiento2021Filtrado[c('AGNO','MRUN','RBD','NOM_RBD','NOM_COM_ALU','NOM_COM_RBD','COD_DEPE','COD_GRADO','RURAL_RBD','LET_CUR','COD_JOR','GEN_ALU','FEC_NAC_ALU','EDAD_ALU','PROM_GRAL','ASISTENCIA')]
+FRendimiento2022Filtrado <- Rendimiento2022Filtrado[c('AGNO','MRUN','RBD','NOM_RBD','NOM_COM_ALU','NOM_COM_RBD','COD_DEPE','COD_GRADO','RURAL_RBD','LET_CUR','COD_JOR','GEN_ALU','FEC_NAC_ALU','EDAD_ALU','PROM_GRAL','ASISTENCIA')]
 
 write.csv2(FRendimiento2019Filtrado, file = "FRendimiento2019Filtrado.csv", row.names = FALSE,fileEncoding = "UTF-8")
 write.csv2(FRendimiento2020Filtrado, file = "FRendimiento2020Filtrado.csv", row.names = FALSE,fileEncoding = "UTF-8")
